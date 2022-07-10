926947cfc6e4dc0ee059bfff5962856a17d170d6
Brandon Roberts <robertsbt@gmail.com>
2022-07-09 19:23:01 -0500

    refactor: update environment for apps

--- apps/analog-app/src/main.ts
+++ apps/analog-app/src/main.ts
@@ -2,10 +2,9 @@ import './polyfills';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 
-if (environment.production) {
+if (import.meta.env.PROD) {
   enableProdMode();
 }
 
