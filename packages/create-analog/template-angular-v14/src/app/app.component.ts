13b922b80329e28106fd10b31802f634f9c11a60
Brandon Roberts <robertsbt@gmail.com>
2022-07-25 11:39:24 -0500

    refactor: update app template

--- packages/create-analog/template-angular-v14/src/app/app.component.ts
+++ packages/create-analog/template-angular-v14/src/app/app.component.ts
@@ -6,30 +6,55 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://angular.io/" target="_blank">
+      <img alt="Angular Logo" class="logo angular" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+      </a>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+    
+    <h1>Vite + Angular</h1>
+
+    <div class="card">
+      <button type="button" (click)="increment()">Count {{ count }}</button>
+    </div>
+
+    <p class="read-the-docs">
+      Click on the Vite and Angular logos to learn more.
+    </p>  
+
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [`
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+
+    .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+    }
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.angular:hover {
+      filter: drop-shadow(0 0 2em #42b883aa);
+    }
+    .read-the-docs {
+      color: #888;
+    }    
+  `]
 })
 export class AppComponent {
-  title = 'my-app';
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
 }
--- packages/create-analog/template-angular-v14/vite.config.ts
+++ packages/create-analog/template-angular-v14/vite.config.ts
@@ -4,12 +4,11 @@ import angular from '@analogjs/vite-plugin-angular';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  optimizeDeps: {
-    exclude: ['rxjs']
-  },
+  publicDir: 'assets',
   build: {
-    outDir: `dist/my-app`,
-    emptyOutDir: true
+    outDir: `../dist/my-app`,
+    emptyOutDir: true,
+    target: 'es2020'
   },
   resolve: {
     mainFields: ['module'],
