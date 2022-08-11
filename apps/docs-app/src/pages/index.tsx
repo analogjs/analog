0ea72f596f1ca98a3856d9e63f597f034aacbb3c
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-08-12 00:51:44 +0200

    docs: fix broken link

--- apps/docs-app/src/pages/index.tsx
+++ apps/docs-app/src/pages/index.tsx
@@ -15,11 +15,8 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/docs">
+            Read the docs
           </Link>
         </div>
       </div>
