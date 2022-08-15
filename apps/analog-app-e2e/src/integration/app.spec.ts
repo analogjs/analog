169e89473e2128fd7987f5807f48d621d189f156
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-08-15 15:57:03 +0200

    test: remove generated example Cypress setup

--- apps/analog-app-e2e/src/integration/app.spec.ts
+++ apps/analog-app-e2e/src/integration/app.spec.ts
@@ -4,10 +4,6 @@ describe('analog-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('My Store');
   });
 });
