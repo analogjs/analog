cfa2ad1c448e626a0c471f6ab1ecef1e4628be7a
Dale Nguyen <dungnq@itbox4vn.com>
2022-08-18 19:01:07 +0000

    fix(38): improve styling for buttons on mobile

--- apps/docs-app/src/pages/index.tsx
+++ apps/docs-app/src/pages/index.tsx
@@ -16,13 +16,13 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs">
+          <Link className="button button--secondary button--lg margin-bottom--md" to="/docs">
             Read the docs
           </Link>
 
           <StackblitzButton
             className={clsx(
-              'margin-left--md button--outline',
+              'margin-left--md button--outline margin-bottom--md',
               styles.buttonOutline
             )}
           />
