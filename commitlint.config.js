a2e2a582cd4e7bff83c8a200c1e28e8289ffe33e
Umair Hafeez <umairhafeez.ca@gmail.com>
2022-08-20 16:54:55 -0400

    chore: add config rules to match analog's requirements
    This will enforce leading blank line before body and footer.
    This will also enforce the scopes as mentioned in the guildeline.
    The scope can be either empty, or one of the specified values.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'scope-enum': [2, 'always', ['vite-angular-plugin', 'create-analog']],
+  },
 };
