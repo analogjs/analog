700558b4e3b132d214580a9f58cf91ffdc6d471a
Maina Wycliffe <wmmaina7@gmail.com>
2022-08-23 21:57:28 +0300

    style(astro-integration-angular): add note for viteAngular as any workaround

--- packages/astro-integration-angular/src/index.ts
+++ packages/astro-integration-angular/src/index.ts
@@ -15,6 +15,17 @@ function getViteConfiguration() {
       include: ['@analogjs/astro-integration-angular/client.js'],
       exclude: ['@analogjs/astro-integration-angular/server.js'],
     },
+    /**
+     *
+     * Why I am casting viteAngular as any
+     *
+     * The vite angular plugins is shipped as commonjs, while this astro
+     * integration is shipped using ESM and if you call the the default
+     * function, you get the following error: viteAngular is not a function.
+     * Attempt to use ESM for the angular vite plugin broke something, hence
+     * this workaround for now.
+     *
+     */
     plugins: [(viteAngular as any).default()],
   };
 }
