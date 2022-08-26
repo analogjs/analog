1f295396c2237dfd6a10d3be7d59476489c8ebef
Brandon Roberts <robertsbt@gmail.com>
2022-08-26 07:38:57 -0500

    chore: fix commit lint scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['vite-angular-plugin', 'create-analog', 'astro-integration-angular'],
+      ['vite-plugin-angular', 'create-analog', 'astro-integration-angular'],
     ],
   },
 };
