cbf9f579b73d4d8ee752ff8afb3088e719049a45
Brandon Roberts <robertsbt@gmail.com>
2022-10-10 13:20:28 -0500

    chore: remove console.log

--- apps/analog-app/src/main.ts
+++ apps/analog-app/src/main.ts
@@ -2,14 +2,14 @@ import './polyfills';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { provideFileRouter, routes } from '@analogjs/router';
+import { provideFileRouter } from '@analogjs/router';
 
 import { AppComponent } from './app/app.component';
 
 if (import.meta.env.PROD) {
   enableProdMode();
 }
-console.log(routes);
+
 bootstrapApplication(AppComponent, {
   providers: [provideFileRouter(), importProvidersFrom(HttpClientModule)],
 }).catch((err) => console.error(err));
