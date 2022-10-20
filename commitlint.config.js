5010117dc0a1d718fcf7c1645c8c15686060386d
Brandon <robertsbt@gmail.com>
2022-10-20 14:10:22 -0500

    docs(router): add initial documentation for router package (#117)

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['vite-plugin-angular', 'create-analog', 'astro-angular'],
+      ['vite-plugin-angular', 'create-analog', 'astro-angular', 'router'],
     ],
   },
 };
