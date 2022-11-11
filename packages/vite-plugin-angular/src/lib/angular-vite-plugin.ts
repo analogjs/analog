a993a86eb68430e3ea4fc6c320b04ce2eaae19ad
Miloš Lajtman <lajtman@gmail.com>
2022-11-11 19:51:30 +0100

    fix(vite-plugin-angular): fix parsing of tsconfig option (#135)
    Closes #134

--- packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
@@ -44,9 +44,10 @@ export function angular(options?: PluginOptions): Plugin[] {
    */
   const pluginOptions = {
     tsconfig:
-      options?.tsconfig ?? process.env['NODE_ENV'] === 'test'
+      options?.tsconfig ??
+      (process.env['NODE_ENV'] === 'test'
         ? './tsconfig.spec.json'
-        : './tsconfig.app.json',
+        : './tsconfig.app.json'),
     workspaceRoot: options?.workspaceRoot ?? process.cwd(),
     inlineStylesExtension: options?.inlineStylesExtension ?? '',
   };
