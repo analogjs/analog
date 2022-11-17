e03def7fe0ff90a783033de7c83690e5d348b33e
Brandon Roberts <robertsbt@gmail.com>
2022-11-17 07:58:23 -0600

    feat(router): add support for named index routes
    Closes #148

--- packages/router/src/lib/routes.spec.ts
+++ packages/router/src/lib/routes.spec.ts
@@ -159,6 +159,49 @@ describe('routes', () => {
     });
   });
 
+  describe('a named index route', () => {
+    const files: Files = {
+      '/app/routes/(home).ts': () =>
+        Promise.resolve({ default: RouteComponent }),
+    };
+
+    const routes = getRoutes(files);
+    const route: ModuleRoute = routes[0];
+
+    it('should have a path', () => {
+      expect(route.path).toBeDefined();
+    });
+
+    it('should have a pathMatch set to full', () => {
+      expect(route.pathMatch).toBe('full');
+    });
+
+    it('should have a _module property', () => {
+      expect(route._module).toBeDefined();
+
+      expect(typeof route._module).toBe('function');
+    });
+
+    it('should have a loadChildren property', () => {
+      expect(route.loadChildren).toBeDefined();
+
+      expect(typeof route.loadChildren).toBe('function');
+    });
+
+    it('should return an array of one route config from the loadChildren property', async () => {
+      expect(route.loadChildren).toBeDefined();
+
+      const routes = (await route.loadChildren()) as Route[];
+
+      expect(routes.length).toBe(1);
+
+      const innerRoute = routes.shift();
+
+      expect(innerRoute.path).toBe('');
+      expect(innerRoute.component).toBe(RouteComponent);
+    });
+  });
+
   describe('a parent/child route', () => {
     const files: Files = {
       '/app/routes/products.ts': () =>
