6fbc2514625be602a53738449e7f687f6e245328
Brandon Roberts <robertsbt@gmail.com>
2022-11-17 21:32:50 -0600

    fix: update template application to register router and initial client/server routes

--- packages/create-analog/template-angular-v14/src/app/app.component.spec.ts
+++ packages/create-analog/template-angular-v14/src/app/app.component.spec.ts
@@ -5,29 +5,13 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        AppComponent
-      ]
+      imports: [RouterTestingModule, AppComponent],
     }).compileComponents();
   });
-  
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have an initial count of 0`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.count).toEqual(0);
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Vite + Angular');
   });
 });
