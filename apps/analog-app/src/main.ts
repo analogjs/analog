c0c6b4397cc5c6c77dd9bd7b199b22f6e3281b5b
Brandon Roberts <robertsbt@gmail.com>
2022-11-23 21:52:41 -0600

    chore: analog-app cleanup

--- apps/analog-app/src/main.ts
+++ apps/analog-app/src/main.ts
@@ -1,14 +1,10 @@
-import './polyfills';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import 'zone.js';
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { provideFileRouter } from '@analogjs/router';
 
 import { AppComponent } from './app/app.component';
-
-if (import.meta.env.PROD) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [provideFileRouter(), importProvidersFrom(HttpClientModule)],
