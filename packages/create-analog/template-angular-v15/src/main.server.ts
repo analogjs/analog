1869a3c49a5aa86387087e2dace619cd848b5163
Brandon Roberts <robertsbt@gmail.com>
2022-12-29 10:47:18 -0600

    feat(create-analog): add SSR support to v15 template app

---
+++ packages/create-analog/template-angular-v15/src/main.server.ts
@@ -0,0 +1,22 @@
+import 'zone.js/node';
+import { enableProdMode } from '@angular/core';
+import { renderApplication } from '@angular/platform-server';
+import { provideFileRouter } from '@analogjs/router';
+import { withEnabledBlockingInitialNavigation } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+
+if (import.meta.env.PROD) {
+  enableProdMode();
+}
+
+export default async function render(url: string, document: string) {
+  const html = await renderApplication(AppComponent, {
+    appId: 'analog-app',
+    document,
+    url,
+    providers: [provideFileRouter(withEnabledBlockingInitialNavigation())],
+  });
+
+  return html;
+}
