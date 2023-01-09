b3d643bfc0eb817694914ad05d8994ea63be77a5
Brandon Roberts <robertsbt@gmail.com>
2023-01-09 07:43:22 -0600

    fix(platform): keep router package from being eagerly optimized

--- packages/platform/src/lib/router-plugin.ts
+++ packages/platform/src/lib/router-plugin.ts
@@ -19,7 +19,7 @@ export function routerPlugin(): Plugin[] {
           },
           optimizeDeps: {
             include: ['rxjs'],
-            exclude: ['@angular/platform-server'],
+            exclude: ['@angular/platform-server', '@analogjs/router'],
           },
         };
       },
