f70a7c304f0aa8e299ad3b4425f67960acb7576e
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-02-05 12:56:21 +0100

    test(content): provide additional test for slug matching

--- packages/content/src/lib/content-files-list-token.spec.ts
+++ packages/content/src/lib/content-files-list-token.spec.ts
@@ -5,6 +5,9 @@ vi.mock('./get-content-files', () => {
   return {
     getContentFilesList: () => ({
       '/test.md': { title: 'Test' },
+      '/path/to/test.md': { title: 'Test' },
+      '\\path\\to\\test.md': { title: 'Test' },
+      '/path/to/test with spaces.md': { title: 'Test' },
     }),
   };
 });
@@ -14,7 +17,29 @@ describe('CONTENT_FILES_LIST_TOKEN', () => {
     const firstParsedFile = CONTENT_FILES_LIST_TOKEN[0];
     expect(CONTENT_FILES_LIST_TOKEN).toBeTruthy();
     expect(firstParsedFile.filename).toEqual('/test.md');
+    expect(firstParsedFile.slug).toEqual('test');
     expect(firstParsedFile.attributes['title']).toEqual('Test');
+  });
+
+  it('should extract the slug from nested path', () => {
+    const { CONTENT_FILES_LIST_TOKEN } = setup();
+    const firstParsedFile = CONTENT_FILES_LIST_TOKEN[1];
+    expect(CONTENT_FILES_LIST_TOKEN).toBeTruthy();
+    expect(firstParsedFile.slug).toEqual('test');
+  });
+
+  it('should extract the slug from nested path (windows)', () => {
+    const { CONTENT_FILES_LIST_TOKEN } = setup();
+    const firstParsedFile = CONTENT_FILES_LIST_TOKEN[2];
+    expect(CONTENT_FILES_LIST_TOKEN).toBeTruthy();
+    expect(firstParsedFile.slug).toEqual('test');
+  });
+
+  it('should extract the slug without spaces', () => {
+    const { CONTENT_FILES_LIST_TOKEN } = setup();
+    const firstParsedFile = CONTENT_FILES_LIST_TOKEN[3];
+    expect(CONTENT_FILES_LIST_TOKEN).toBeTruthy();
+    expect(firstParsedFile.slug).toEqual('test%20with%20spaces');
   });
 
   function setup() {
