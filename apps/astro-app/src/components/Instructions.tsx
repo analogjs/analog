b2351974800c656110ef0be9d2e88b8d59229deb
Brandon Roberts <robertsbt@gmail.com>
2023-02-06 21:34:50 -0600

    chore: add React component to astro-app to ensure compatibility

---
+++ apps/astro-app/src/components/Instructions.tsx
@@ -0,0 +1,12 @@
+export const Instructions = () => {
+  return (
+    <>
+      <p className="instructions">
+        Check out the <code>src/pages</code> directory to get started.
+        <br />
+        <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message
+        above.
+      </p>
+    </>
+  );
+};
