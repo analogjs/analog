0b4c962a7313f1a495a916154c2168dcd8aa1384
Brandon Roberts <robertsbt@gmail.com>
2023-04-10 07:49:17 -0500

    fix(platform): use slash in event handler for apiPrefix

--- packages/platform/src/lib/runtime/api-middleware.js
+++ packages/platform/src/lib/runtime/api-middleware.js
@@ -9,7 +9,7 @@
 import { eventHandler } from 'h3';
 
 export default eventHandler(async (event) => {
-  const apiPrefix = import.meta.env.RUNTIME_CONFIG?.apiPrefix ?? '/api';
+  const apiPrefix = `/${import.meta.env.RUNTIME_CONFIG?.apiPrefix ?? 'api'}`;
   if (event.req.url?.startsWith(apiPrefix)) {
     return $fetch(`${event.req.url?.replace(apiPrefix, '')}`);
   }
