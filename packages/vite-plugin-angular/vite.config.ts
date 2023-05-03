c97ff33ca9312bb4dca4498ad732a528bf23a9e9
Brandon Roberts <robertsbt@gmail.com>
2023-05-03 16:54:54 -0500

    fix: add vite config for vite-plugin-angular tests

---
+++ packages/vite-plugin-angular/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+
+import { offsetFromRoot } from '@nx/devkit';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    test: {
+      globals: true,
+      environment: 'node',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cache: {
+        dir: `${offsetFromRoot('packages/platform/src')}/node_modules/.vitest`,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
