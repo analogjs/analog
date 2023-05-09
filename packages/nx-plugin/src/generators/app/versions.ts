7b39c21a2a36e96f2c73821cf066d8bf51983142
Brandon Roberts <robertsbt@gmail.com>
2023-05-08 21:19:15 -0500

    fix(create-analog): update Vitest for Nx 16.x

--- packages/nx-plugin/src/generators/app/versions.ts
+++ packages/nx-plugin/src/generators/app/versions.ts
@@ -1,9 +1,9 @@
 export const MINIMUM_SUPPORTED_ANGULAR_VERSION = '15.0.0';
 // V16
 // dependencies
-export const V16_ANGULAR = '16.0.0';
+export const V16_ANGULAR = '^16.0.0';
 export const V16_NX_DEVKIT = '^16.0.0';
-export const V16_NX_ANGULAR = '16.1.0-rc.0';
+export const V16_NX_ANGULAR = '^16.0.0';
 export const V16_ANALOG_JS_CONTENT = '^0.2.0-beta.4';
 export const V16_ANALOG_JS_ROUTER = '^0.2.0-beta.4';
 export const V16_ANGULAR_PLATFORM_SERVER = '^16.0.0';
@@ -18,22 +18,22 @@ export const V16_JSDOM = '^20.0.0';
 export const V16_TYPESCRIPT = '~5.0.2';
 export const V16_VITE = '^4.0.3';
 export const V16_VITE_TSCONFIG_PATHS = '^4.0.2';
-export const V16_VITEST = '^0.25.8';
+export const V16_VITEST = '^0.31.0';
 
 // V15
 // dependencies
 export const V15_ANGULAR = '15.2.7';
 export const V15_NRWL_DEVKIT = '15.9.2';
 export const V15_NRWL_ANGULAR = '15.9.2';
-export const V15_ANALOG_JS_CONTENT = '0.1.9';
-export const V15_ANALOG_JS_ROUTER = '0.1.0-alpha.10';
+export const V15_ANALOG_JS_CONTENT = '^0.2.0-beta.4';
+export const V15_ANALOG_JS_ROUTER = '^0.2.0-beta.4';
 export const V15_ANGULAR_PLATFORM_SERVER = '^15.0.0';
 export const V15_FRONT_MATTER = '^4.0.2';
 export const V15_MARKED = '^4.2.12';
 export const V15_PRISMJS = '^1.29.0';
 
 // devDependencies
-export const V15_ANALOG_JS_PLATFORM = '0.1.0-beta.22';
+export const V15_ANALOG_JS_PLATFORM = '0.1.0-beta.23';
 export const V15_NRWL_VITE = '^15.7.0';
 export const V15_JSDOM = '^20.0.0';
 export const V15_TYPESCRIPT = '~4.8.4';
