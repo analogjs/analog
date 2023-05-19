6da21fd02f4000ee223baa5bbd107eb9d0f2c56e
Brandon Roberts <robertsbt@gmail.com>
2023-05-19 07:18:09 -0500

    chore: release 0.2.0-beta.9

---
+++ tools/publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm publish node_modules/@analogjs/astro-angular --access public
+npm publish node_modules/@analogjs/content --access public
+npm publish node_modules/@analogjs/platform --access public
+npm publish node_modules/@analogjs/router --access public
+npm publish node_modules/@analogjs/trpc --access public
+npm publish node_modules/@analogjs/vite-plugin-angular --access public
+npm publish node_modules/@analogjs/vite-plugin-nitro --access public
+npm publish dist/packages/create-analog
