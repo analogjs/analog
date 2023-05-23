096b45e5a65269fd6a378c1ee69d7610c560b521
Brandon Roberts <robertsbt@gmail.com>
2023-05-22 22:56:45 -0500

    fix(content): render markdown content in resolver for markdown route (#415)

---
+++ packages/content/src/lib/markdown-route.component.ts
@@ -0,0 +1,38 @@
+import { AsyncPipe } from '@angular/common';
+import {
+  AfterViewChecked,
+  Component,
+  inject,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+
+import { ContentRenderer } from './content-renderer';
+import { AnchorNavigationDirective } from './anchor-navigation.directive';
+
+@Component({
+  selector: 'analog-markdown-route',
+  standalone: true,
+  imports: [AsyncPipe],
+  hostDirectives: [AnchorNavigationDirective],
+  preserveWhitespaces: true,
+  encapsulation: ViewEncapsulation.None,
+  template: `<div [innerHTML]="content" [class]="classes"></div>`,
+})
+export default class AnalogMarkdownRouteComponent implements AfterViewChecked {
+  private sanitizer = inject(DomSanitizer);
+  private route = inject(ActivatedRoute);
+  contentRenderer = inject(ContentRenderer);
+
+  protected content: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(
+    this.route.snapshot.data['renderedAnalogContent']
+  );
+
+  @Input() classes = 'analog-markdown-route';
+
+  ngAfterViewChecked() {
+    this.contentRenderer.enhance();
+  }
+}
