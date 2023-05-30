f594e5746af07b77f88a2f11e801ebd1e1e2e585
Brandon Roberts <robertsbt@gmail.com>
2023-05-30 10:11:56 -0500

    chore: fix trpc-app

--- apps/trpc-app/src/app.config.ts
+++ apps/trpc-app/src/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 import { provideTRPCClient } from './trpc-client';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFileRouter(),
+    provideHttpClient(),
     provideClientHydration(),
     provideTRPCClient(),
   ],
