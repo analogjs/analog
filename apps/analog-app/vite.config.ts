01acacff15b79d99c65b86076fac3304c7908d9f
Brandon Roberts <robertsbt@gmail.com>
2023-06-08 15:43:17 -0500

    ci: generate VITE_ANALOG_PUBLIC_BASE_URL for Netlify deployment

--- apps/analog-app/vite.config.ts
+++ apps/analog-app/vite.config.ts
@@ -4,6 +4,23 @@ import analog from '@analogjs/platform';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig, Plugin, splitVendorChunkPlugin } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Only run in CI
+if (process.env['CI'] === 'true') {
+  let base = process.env['URL'];
+
+  if (process.env['CONTEXT'] === 'deploy-preview') {
+    base = `${process.env['DEPLOY_PRIME_URL']}/`;
+  }
+
+  // write BASE_URL env variable to .env file
+  fs.writeFileSync(
+    path.resolve(__dirname, '.env.local'),
+    `VITE_ANALOG_PUBLIC_BASE_URL=${base}`
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
