8a432a9dd1c5cdd5496a9f9d7543b496fd0daec5
Robin Goetz <35136007+goetzrobin@users.noreply.github.com>
2023-06-09 14:34:11 +0200

    feat(trpc): make CreateTRPCProxyClient publicly available (#453)

--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/client/client';
 export * from './lib/server/server';
 export * from './lib/utils/wait-for';
+
+export { CreateTrpcProxyClient } from './lib/client/trpc-rxjs-proxy';
