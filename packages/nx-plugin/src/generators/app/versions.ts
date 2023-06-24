8cb1cb680783f0d6bcb25242e66119151ca0896d
Brandon Roberts <robertsbt@gmail.com>
2023-06-24 13:10:09 -0500

    feat: update template app dependencies to Angular v16.1.x, Nx 16.4.x

--- packages/nx-plugin/src/generators/app/versions.ts
+++ packages/nx-plugin/src/generators/app/versions.ts
@@ -1,30 +1,30 @@
 export const MINIMUM_SUPPORTED_ANGULAR_VERSION = '15.0.0';
 // V16
 // dependencies
-export const V16_ANGULAR = '16.0.0';
-export const V16_NX_DEVKIT = '~16.0.0';
-export const V16_NX_ANGULAR = '~16.0.0';
-export const V16_ANALOG_JS_CONTENT = '0.2.0-beta.15';
-export const V16_ANALOG_JS_ROUTER = '0.2.0-beta.15';
-export const V16_ANALOG_JS_TRPC = '0.2.0-beta.15';
+export const V16_ANGULAR = '16.1.0';
+export const V16_NX_DEVKIT = '~16.4.0';
+export const V16_NX_ANGULAR = '~16.4.0';
+export const V16_ANALOG_JS_CONTENT = '0.2.0-beta.17';
+export const V16_ANALOG_JS_ROUTER = '0.2.0-beta.17';
+export const V16_ANALOG_JS_TRPC = '0.2.0-beta.17';
 export const V16_TRPC_CLIENT = '^10.25.0';
 export const V16_TRPC_SERVER = '^10.25.0';
 export const V16_ISOMORPHIC_FETCH = '^3.0.0';
 export const V16_SUPERJSON = '^1.12.3';
 
-export const V16_ANGULAR_PLATFORM_SERVER = '~16.0.0';
+export const V16_ANGULAR_PLATFORM_SERVER = '~16.1.0';
 export const V16_FRONT_MATTER = '^4.0.2';
 export const V16_MARKED = '^5.0.2';
 export const V16_PRISMJS = '^1.29.0';
 
 // devDependencies
-export const V16_ANALOG_JS_PLATFORM = '0.2.0-beta.15';
-export const V16_NX_VITE = '^16.0.0';
-export const V16_JSDOM = '^20.0.0';
+export const V16_ANALOG_JS_PLATFORM = '0.2.0-beta.17';
+export const V16_NX_VITE = '^16.4.0';
+export const V16_JSDOM = '^22.1.0';
 export const V16_TYPESCRIPT = '~5.0.2';
-export const V16_VITE = '^4.0.3';
+export const V16_VITE = '^4.3.9';
 export const V16_VITE_TSCONFIG_PATHS = '^4.0.2';
-export const V16_VITEST = '^0.31.0';
+export const V16_VITEST = '^0.32.0';
 export const V16_ZOD = '^3.21.4';
 
 // V15
