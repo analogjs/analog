b4650e57cf54a4f708d76b793ef80a89e280f3af
Brandon Roberts <robertsbt@gmail.com>
2023-10-23 22:56:26 -0500

    chore: cleanup

--- packages/nx-plugin/src/generators/app/lib/add-analog-project-config.ts
+++ packages/nx-plugin/src/generators/app/lib/add-analog-project-config.ts
@@ -66,7 +66,7 @@ export function addAnalogProjectConfig(
         },
       },
       test: {
-        executor: `@nx/vite:test`,
+        executor: `@analogjs/platform:vitest`,
         outputs: [`{projectRoot}/coverage`],
       },
     },
