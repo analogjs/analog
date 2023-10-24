365809799e61baab5cf5ca48c85af62a3884f230
Brandon Roberts <robertsbt@gmail.com>
2023-10-23 22:44:54 -0500

    chore: update project executors

--- packages/nx-plugin/src/generators/app/lib/add-analog-project-config.ts
+++ packages/nx-plugin/src/generators/app/lib/add-analog-project-config.ts
@@ -66,7 +66,7 @@ export function addAnalogProjectConfig(
         },
       },
       test: {
-        executor: `@analogjs/platform:vitest`,
+        executor: `@nx/vite:test`,
         outputs: [`{projectRoot}/coverage`],
       },
     },
