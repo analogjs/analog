ef5bdd053f1c983d58519185f3d72a283a84368e
Chris <perko@herodevs.com>
2023-11-19 22:01:47 -0500

    fix: uses routerLink instead of href

--- packages/create-analog/template-blog/src/app/pages/blog/index.page.ts
+++ packages/create-analog/template-blog/src/app/pages/blog/index.page.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { injectContentFiles } from '@analogjs/content';
 import PostAttributes from '../../post-attributes';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
+  imports: [RouterLink],
   template: `
     <h1>Blog Archive</h1>
-    @for (post of posts; track post.attributes.slug) {
-    <a [href]="'/blog/' + post.attributes.slug">
+    @for (post of posts;track post.attributes.slug) {
+    <a [routerLink]="['/blog/', post.attributes.slug]">
       <h2 class="post__title">{{ post.attributes.title }}</h2>
       <p class="post__desc">{{ post.attributes.description }}</p>
     </a>
