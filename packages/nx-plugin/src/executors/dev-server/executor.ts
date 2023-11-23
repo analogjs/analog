d1a8163126d42672d31678dbafe5e0a2f9954cba
Brandon Roberts <robertsbt@gmail.com>
2023-11-23 00:21:56 -0600

    fix: use project config

--- packages/nx-plugin/src/executors/dev-server/executor.ts
+++ packages/nx-plugin/src/executors/dev-server/executor.ts
@@ -6,21 +6,16 @@ import {
   DevServerBuilderOptions,
   ApplicationBuilderOptions,
 } from '@angular-devkit/build-angular';
-import { createEvent } from 'h3';
 
-import { InlineStyleLanguage } from '@angular-devkit/build-angular/src/builders/application/schema';
 import { loadEsmModule } from '@angular-devkit/build-angular/src/utils/load-esm';
 import { NitroConfig } from 'nitropack';
 import { Connect, normalizePath } from 'vite';
-
-import { DevServerExecutorSchema } from './schema';
+import { createEvent } from 'h3';
 
 export default async function* runExecutor(
-  options: DevServerExecutorSchema,
+  options: DevServerBuilderOptions,
   context: ExecutorContext
 ) {
-  // console.log('Executor ran for DevServer', context.root);
-
   const builderContext = await createBuilderContext(
     {
       builderName: '@angular-devkit/build-angular:application',
@@ -35,23 +30,11 @@ export default async function* runExecutor(
   builderContext.getBuilderNameForTarget = () =>
     Promise.resolve('@angular-devkit/build-angular:application');
 
-  builderContext.getTargetOptions = () => {
-    const appBuilderOptions: ApplicationBuilderOptions = {
-      aot: true,
-      browser: 'apps/analog-app/src/main.ts',
-      index: 'apps/analog-app/index.html',
-      outputPath: 'dist/apps/analog-app/client',
-      tsConfig: 'apps/analog-app/tsconfig.app.json',
-      progress: true,
-      watch: true,
-      optimization: false,
-      inlineStyleLanguage: InlineStyleLanguage.Scss,
-      sourceMap: {
-        scripts: true,
-        styles: true,
-      },
-      styles: ['apps/analog-app/src/styles.css'],
-    };
+  builderContext.getTargetOptions = (target) => {
+    const appBuilderOptions: ApplicationBuilderOptions =
+      context.projectsConfigurations.projects[target.project].targets[
+        target.target
+      ].options;
 
     return Promise.resolve(appBuilderOptions as any);
   };
@@ -79,13 +62,6 @@ export default async function* runExecutor(
   });
   const server = createDevServer(nitro);
   await build(nitro);
-  const devServerOptions: DevServerBuilderOptions = {
-    buildTarget: 'analog-app:build:development',
-    liveReload: true,
-    port: 4200,
-    forceEsbuild: true,
-    watch: true,
-  };
 
   const nitroApiMiddleware: Connect.NextHandleFunction = async (
     req,
@@ -100,14 +76,9 @@ export default async function* runExecutor(
     next();
   };
 
-  const sub = executeDevServerBuilder(
-    devServerOptions,
-    builderContext,
-    undefined,
-    {
-      middleware: [nitroApiMiddleware],
-    }
-  ).subscribe();
+  const sub = executeDevServerBuilder(options, builderContext, undefined, {
+    middleware: [nitroApiMiddleware],
+  }).subscribe();
 
   builderContext.addTeardown(() => {
     sub.unsubscribe();
