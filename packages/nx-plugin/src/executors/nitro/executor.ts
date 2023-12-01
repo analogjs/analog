07c99b03a56a702c0d37039488018cd50e5c22bf
Brandon Roberts <robertsbt@gmail.com>
2023-11-30 22:06:31 -0600

    feat: nitro executor

--- packages/nx-plugin/src/executors/nitro/executor.ts
+++ packages/nx-plugin/src/executors/nitro/executor.ts
@@ -1,37 +1,45 @@
 import { loadEsmModule } from '@angular-devkit/build-angular/src/utils/load-esm';
-import { NitroExecutorSchema } from './schema';
+import { ExecutorContext } from '@nx/devkit';
 import { NitroConfig } from 'nitropack';
-import { normalizePath } from 'vite';
 import * as path from 'path';
-import * as fs from 'fs';
 
-export default async function runExecutor(options: NitroExecutorSchema) {
-  console.log('Executor ran for Nitro', options);
+import { NitroExecutorSchema } from './schema';
+
+export default async function runExecutor(
+  options: NitroExecutorSchema,
+  context: ExecutorContext
+) {
+  // console.log('Executor ran for Nitro', options);
+  const { normalizePath } = await import('vite');
   const { createNitro, build, prepare, copyPublicAssets, prerender } =
     await loadEsmModule<typeof import('nitropack')>('nitropack');
 
-  const workspaceRoot = process.cwd();
-  const rootDir = 'apps/analog-app';
-  let nitroConfig: NitroConfig = {
+  const projectConfig =
+    context.projectsConfigurations.projects[context.projectName];
+
+  const workspaceRoot = context.root;
+  const rootDir = projectConfig.root;
+  const nitroConfig: NitroConfig = {
     rootDir,
     logLevel: 3,
     srcDir: normalizePath(`${rootDir}/src/server`),
     scanDirs: [normalizePath(`${rootDir}/src/server`)],
     output: {
-      dir: normalizePath(
-        path.resolve(workspaceRoot, 'dist', rootDir, 'analog')
-      ),
+      dir: normalizePath(path.resolve(workspaceRoot, options.outputPath)),
       publicDir: normalizePath(
-        path.resolve(workspaceRoot, 'dist', rootDir, 'analog/public')
+        path.resolve(workspaceRoot, options.staticBuildOutputPath)
+      ),
+      serverDir: normalizePath(
+        path.resolve(workspaceRoot, options.serverBuildOutputPath)
       ),
     },
     publicAssets: [
-      { dir: path.join(workspaceRoot, 'dist/apps/analog-app/client/browser') },
+      { dir: path.join(workspaceRoot, options.buildOutputPath, 'browser') },
     ],
     serverAssets: [
       {
         baseName: 'public',
-        dir: path.join(workspaceRoot, 'dist/apps/analog-app/client/browser'),
+        dir: path.join(workspaceRoot, options.buildOutputPath, 'browser'),
       },
     ],
     handlers: [
@@ -43,21 +51,22 @@ export default async function runExecutor(options: NitroExecutorSchema) {
         middleware: true,
       },
     ],
-    renderer: path.join(
-      workspaceRoot,
-      'packages/vite-plugin-nitro/src/lib/runtime/renderer'
-    ),
+    renderer: path.join(workspaceRoot, options.runtimeRenderer),
     alias: {
       '#analog/ssr': normalizePath(
         path.join(
           workspaceRoot,
-          'dist/apps/analog-app/client/server/main.server'
+          options.buildOutputPath,
+          'server',
+          'main.server'
         )
       ),
       '#analog/index': normalizePath(
         path.join(
           workspaceRoot,
-          'dist/apps/analog-app/client/server/index.server.html'
+          options.buildOutputPath,
+          'server',
+          'index.server.html'
         )
       ),
     },
@@ -65,25 +74,12 @@ export default async function runExecutor(options: NitroExecutorSchema) {
 
   const nitro = await createNitro({
     dev: false,
-    preset: 'node-server',
+    preset: options.preset,
     ...nitroConfig,
   });
 
   await prepare(nitro);
   await copyPublicAssets(nitro);
-
-  if (
-    nitroConfig?.prerender?.routes &&
-    nitroConfig?.prerender?.routes.find((route) => route === '/')
-  ) {
-    // Remove the root index.html so it can be replaced with the prerendered version
-    if (fs.existsSync(`${nitroConfig?.output?.publicDir}/index.html`)) {
-      console.log(`rm ${nitroConfig?.output?.publicDir}/index.html`);
-      fs.unlinkSync(`${nitroConfig?.output?.publicDir}/index.html`);
-    }
-  }
-
-  await prerender(nitro);
   await build(nitro);
 
   return {
--- packages/nx-plugin/src/executors/nitro/schema.d.ts
+++ packages/nx-plugin/src/executors/nitro/schema.d.ts
@@ -1,1 +1,8 @@
-export interface NitroExecutorSchema {} // eslint-disable-line
+export interface NitroExecutorSchema {
+  preset: string;
+  buildOutputPath: string;
+  outputPath: string;
+  staticBuildOutputPath: string;
+  serverBuildOutputPath: string;
+  runtimeRenderer: string;
+} // eslint-disable-line
