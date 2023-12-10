c1b8211153d5259214a5a5f807fb892f82a28600
Brandon Roberts <robertsbt@gmail.com>
2023-12-09 20:34:53 -0600

    fix: set beta tag on publish

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo $TAG
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
-npm publish node_modules/@analogjs/astro-angular --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/content --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/platform --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/router --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/trpc --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/vite-plugin-angular --access public --tag $TAG --dry-run
-npm publish node_modules/@analogjs/vite-plugin-nitro --access public --tag $TAG --dry-run
-npm publish dist/packages/create-analog --tag $TAG --dry-run
+npm publish node_modules/@analogjs/astro-angular --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/content --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/platform --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/router --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/trpc --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/vite-plugin-angular --access public --tag beta --dry-run
+npm publish node_modules/@analogjs/vite-plugin-nitro --access public --tag beta --dry-run
+npm publish dist/packages/create-analog --tag beta --dry-run
