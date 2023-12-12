dac85341df693c0f55f67290461f5d9acedd0c0b
Brandon Roberts <robertsbt@gmail.com>
2023-12-12 07:58:01 -0600

    chore: release 1.0.0-beta.0

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Publishing to $RELEASE_TAG"
-npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
-npm publish node_modules/@analogjs/astro-angular --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/content --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/platform --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/router --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/trpc --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/vite-plugin-angular --access public --tag $RELEASE_TAG --dry-run
-npm publish node_modules/@analogjs/vite-plugin-nitro --access public --tag $RELEASE_TAG --dry-run
-# npm publish dist/packages/create-analog --tag $RELEASE_TAG --dry-run
+#npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
+npm publish node_modules/@analogjs/astro-angular --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/content --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/platform --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/router --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/trpc --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/vite-plugin-angular --access public --tag $RELEASE_TAG
+npm publish node_modules/@analogjs/vite-plugin-nitro --access public --tag $RELEASE_TAG
+npm publish dist/packages/create-analog --tag $RELEASE_TAG
