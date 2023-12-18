b13c7bd4fa7d0c1a784719d94284e6ae6a38c1d7
Jeffrey Smith <jsmith6690@gmail.com>
2023-12-18 16:23:38 -0500

    feat(vite-plugin-nitro): provide req and res to renderer
    PR adds full req and res object from h3 to the analog event handler for  app rendering
    
    closes #802

---
+++ apps/analog-app/src/app/interceptors/cookies.interceptor.ts
@@ -0,0 +1,23 @@
+import { isPlatformServer } from '@angular/common';
+import { HttpHandlerFn, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { PLATFORM_ID, inject } from '@angular/core';
+
+export function cookieInterceotor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+  location = inject(PLATFORM_ID)
+) {
+  if (isPlatformServer(location)) {
+    let headers = new HttpHeaders();
+    const cookies = req.headers.get('cookie');
+    headers = headers.set('cookie', cookies ?? '');
+
+    const cookiedRequest = req.clone({
+      headers,
+    });
+
+    return next(cookiedRequest);
+  } else {
+    return next(req);
+  }
+}
--- packages/vite-plugin-nitro/src/lib/runtime/renderer.js
+++ packages/vite-plugin-nitro/src/lib/runtime/renderer.js
@@ -12,7 +12,11 @@ import renderer from '#analog/ssr';
 import template from '#analog/index';
 
 export default eventHandler(async (event) => {
-  const html = await renderer(event.node.req.url, template);
-
+  const html = await renderer(
+    event.node.req.url,
+    template,
+    event.node.req,
+    event.node.res
+  );
   return html;
 });
