0ac68173e70f6599ea06e5d12258b62822190aeb
Brandon Roberts <robertsbt@gmail.com>
2024-01-01 21:21:57 -0600

    chore: add experimental types to template app

--- packages/create-analog/template-angular-v17/src/vite-env.d.ts
+++ packages/create-analog/template-angular-v17/src/vite-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="vite/client" />
+
+// Uncomment the lines below to enable types for experimental .ng format support
+// declare global {
+//   import type { Component, Directive, Pipe } from '@angular/core';
+
+//   interface Window {
+//     defineComponentMetadata: (
+//       metadata: Omit<
+//         Component,
+//         | 'template'
+//         | 'templateUrl'
+//         | 'host'
+//         | 'standalone'
+//         | 'changeDetection'
+//         | 'styleUrls'
+//         | 'styleUrl'
+//         | 'styles'
+//       >
+//     ) => void;
+//     defineDirectiveMetadata: (
+//       metadata: Omit<Directive, 'host' | 'standalone'>
+//     ) => void;
+//     definePipeMetadata: (metadata: Omit<Pipe, 'standalone'>) => void;
+//   }
+// }
+
+// declare module '*.ng' {
+//   const cmp = any;
+//   export default cmp;
+// }
