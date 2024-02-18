0eea9ffa22bf0f989e42f9821d906ec2dedadb7b
Brandon Roberts <robertsbt@gmail.com>
2024-02-18 14:48:31 -0600

    refactor: move route discovery paths into plugin

--- packages/platform/src/lib/router-plugin.ts
+++ packages/platform/src/lib/router-plugin.ts
@@ -81,5 +81,64 @@ export function routerPlugin(): Plugin[] {
         server.watcher.on('unlink', invalidateRoutes);
       },
     },
+    {
+      name: 'analog-glob-routes',
+      transform(code, id) {
+        if (
+          code.includes('ANALOG_ROUTE_FILES') &&
+          code.includes('ANALOG_CONTENT_FILES') &&
+          id.includes('analogjs')
+        ) {
+          const result = code
+            .replace(
+              'let ANALOG_ROUTE_FILES = {};',
+              `
+            let ANALOG_ROUTE_FILES = import.meta.glob([
+              '/app/routes/**/*.ts',
+              '/src/app/routes/**/*.ts',
+              '/src/app/pages/**/*.page.ts',
+              '/src/app/pages/**/*.page.analog',
+            ]);
+          `
+            )
+            .replace(
+              'let ANALOG_CONTENT_FILES = {};',
+              `
+          let ANALOG_CONTENT_FILES = import.meta.glob(
+            ['/src/app/routes/**/*.md', '/src/app/pages/**/*.md'],
+            { query: '?analog-content-file=true', import: 'default' }
+          );
+          `
+            );
+
+          return {
+            code: result,
+          };
+        }
+
+        return;
+      },
+    },
+    {
+      name: 'analog-glob-endpoints',
+      transform(code, id) {
+        if (code.includes('PAGE_ENDPOINTS') && id.includes('analogjs')) {
+          const result = code.replace(
+            'let PAGE_ENDPOINTS: any = {};',
+            `
+            let PAGE_ENDPOINTS = import.meta.glob([
+              '/src/app/pages/**/*.server.ts',
+            ]);
+          `
+          );
+
+          return {
+            code: result,
+          };
+        }
+
+        return;
+      },
+    },
   ];
 }
