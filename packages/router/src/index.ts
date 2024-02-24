37931bf11c87b4bdfb545a104aec98a1d3ec4704
N. Can KIRIK <can@epati.com.tr>
2024-02-24 16:03:32 +0300

    feat: added exports to be able to write custom router providers

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,4 +1,7 @@
-export { routes } from './lib/routes';
+export type { Files } from './lib/routes';
+export type { RouteExport } from './lib/models';
+
+export { routes, createRoutes } from './lib/routes';
 export {
   defineRouteMeta,
   injectActivatedRoute,
@@ -6,6 +9,6 @@ export {
 } from './lib/define-route';
 export { RouteMeta } from './lib/models';
 export { provideFileRouter } from './lib/provide-file-router';
-export { MetaTag } from './lib/meta-tags';
+export { MetaTag, updateMetaTagsOnRouteChange } from './lib/meta-tags';
 export { PageServerLoad, LoadResult } from './lib/route-types';
 export { injectLoad } from './lib/inject-load';
