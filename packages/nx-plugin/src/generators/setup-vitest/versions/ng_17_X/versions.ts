dceb37e37bb958c1d21a2797290b52a68f34993d
Brandon Roberts <robertsbt@gmail.com>
2024-03-25 07:07:51 -0600

    fix(nx-plugin): rename Vite config and correct Vitest version for Vitest schematic (#965)

--- packages/nx-plugin/src/generators/setup-vitest/versions/ng_17_X/versions.ts
+++ packages/nx-plugin/src/generators/setup-vitest/versions/ng_17_X/versions.ts
@@ -1,8 +1,8 @@
 // V17_X
 
 // devDependencies
-export const V17_X_ANALOG_JS_PLATFORM = '^1.0.0-rc.0';
+export const V17_X_ANALOG_JS_PLATFORM = '^1.0.0';
 export const V17_X_NX_VITE = '^18.0.0';
 export const V17_X_JSDOM = '^22.0.0';
 export const V17_X_VITE_TSCONFIG_PATHS = '^4.2.0';
-export const V17_X_VITEST = '^1.31.1';
+export const V17_X_VITEST = '^1.3.1';
