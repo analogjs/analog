62cc15fe5ab7dc8653980233a3814d1065797f9f
alator21 <botazz133@gmail.com>
2024-03-27 22:09:58 +0200

    fix(astro-angular): enable angular-prod mode at the astro:config:done step
    Enable prod mode only when NODE_ENV is 'production'
    
    Closes #966

--- packages/astro-angular/src/index.ts
+++ packages/astro-angular/src/index.ts
@@ -91,7 +91,9 @@ export default function (options?: AngularOptions): AstroIntegration {
 To disable this warning, set the syntaxHighlight option in your astro.config.mjs mdx() integration to 'prism' or false.`
           );
         }
-        enableProdMode();
+        if (process.env.NODE_ENV === 'production') {
+          enableProdMode();
+        }
       },
     },
   };
