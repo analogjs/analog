d3ca4b50fe598ac1678a7f27d682d55d46df3895
Brandon Roberts <robertsbt@gmail.com>
2024-05-13 11:53:36 -0500

    fix(nx-plugin): adjust templates for adding Analog to v15

--- packages/nx-plugin/src/utils/versions/dependencies.ts
+++ packages/nx-plugin/src/utils/versions/dependencies.ts
@@ -10,6 +10,11 @@ import {
   V17_X_ANALOG_JS_ROUTER,
   V17_X_NX_ANGULAR,
 } from './ng_17_X/versions';
+import {
+  V15_X_ANALOG_JS_CONTENT,
+  V15_X_ANALOG_JS_ROUTER,
+  V15_X_NX_ANGULAR,
+} from './ng_15_X/versions';
 
 const dependencyKeys = [
   '@analogjs/content',
@@ -32,6 +37,15 @@ const getDependencies = (escapedAngularVersion: string) => {
   // fail out for versions <15.2.0
   if (lt(escapedAngularVersion, '15.0.0')) {
     throw new Error(stripIndents`Angular v15.0.0 or newer is required.`);
+  }
+
+  // install 15.x deps for versions <16.0.0
+  if (lt(escapedAngularVersion, '16.0.0')) {
+    return {
+      '@analogjs/content': V15_X_ANALOG_JS_CONTENT,
+      '@analogjs/router': V15_X_ANALOG_JS_ROUTER,
+      '@nx/angular': V15_X_NX_ANGULAR,
+    };
   }
 
   // install 16.x deps for versions <17.0.0
