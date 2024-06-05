d578845c1cfd9ae274092902adb32cf4db584f5b
Pranav Ramesh <pranav.ramesh@snyder.tech>
2024-06-04 20:50:11 -0400

    feat(content): made sponsor logo's horizontal + added spacing

--- apps/docs-app/src/pages/index.tsx
+++ apps/docs-app/src/pages/index.tsx
@@ -46,7 +46,7 @@ function HomepageHeader() {
 
 function SponsorSection() {
   return (
-    <section className={clsx('hero', styles.heroBanner)}>
+    <section className={clsx('hero', styles.heroBanner, styles.horizontal)}>
       <div className="container">
         <h3>
           <Translate
@@ -65,7 +65,7 @@ function SponsorSection() {
           </Translate>
         </p>
 
-        <div>
+        <div className={styles.horizontalSponsors}>
           <a
             href="https://github.blog/2023-04-12-github-accelerator-our-first-cohort-and-whats-next/"
             target="_blank"
@@ -75,28 +75,27 @@ function SponsorSection() {
               width="100px"
               height="100px"
               alt="GitHub Accelerator"
+              style={{ marginRight: '20px' }}
             />
           </a>
-        </div>
 
-        <div>
           <a href="https://nx.dev" target="_blank">
             <img
-              src="https://raw.githubusercontent.com/analogjs/analog/main/apps/docs-app/static/img/logos/nx.svg"
+              src="https://avatars.githubusercontent.com/u/23692104?s=200&v=4"
               width="100px"
               height="100px"
               alt="NxDevTools"
+              style={{ marginRight: '20px' }}
             />
           </a>
-        </div>
 
-          <div>
           <a href="https://snyder.tech" target="_blank">
             <img
               src="https://camo.githubusercontent.com/510635f8c9545f3b94b2a9ca5c8e23c035a3c61f4f9b481a09d799c7209e507f/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f433445304241514734684d764c7a6e74365f512f636f6d70616e792d6c6f676f5f3230305f3230302f302f313633303631383331313034312f736e79646572746563686e6f6c6f676965735f6c6f676f3f653d3231343734383336343726763d6265746126743d54467136564b57416762356f4930466d627661655450467a4d4a52395339345f4c766e4c6e6c5271664c49"
               width="100px"
               height="100px"
               alt="SnyderTechLogo"
+              style={{ marginRight: '20px' }}
             />
           </a>
         </div>
