e0df799ab319ed5294ab961231958354ff47a18a
Ryan Clements <ryanclementshax@gmail.com>
2024-06-17 20:39:36 -0400

    docs: create docs on integrating with storybook

--- apps/docs-app/sidebars.js
+++ apps/docs-app/sidebars.js
@@ -196,6 +196,11 @@ const sidebars = {
           id: 'integrations/ionic/index',
           label: 'Ionic Framework',
         },
+        {
+          type: 'doc',
+          id: 'integrations/storybook/index',
+          label: 'Storybook',
+        },
       ],
     },
     {
