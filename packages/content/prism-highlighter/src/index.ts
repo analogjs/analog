cca1f29b824a28923797e1001920495471181089
Brandon Roberts <robertsbt@gmail.com>
2024-06-22 14:39:47 -0500

    fix(content): import prismjs on client for prism highlighter

--- packages/content/prism-highlighter/src/index.ts
+++ packages/content/prism-highlighter/src/index.ts
@@ -1,5 +1,6 @@
 import { ContentRenderer, NoopContentRenderer } from '@analogjs/content';
 import { Provider } from '@angular/core';
+import 'prismjs';
 
 export function withPrismHighlighter(): Provider[] {
   return [{ provide: ContentRenderer, useClass: NoopContentRenderer }];
