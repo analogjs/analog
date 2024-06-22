c51e472e9cd8f185531d2fc0691fce76abdb2204
Brandon Roberts <robertsbt@gmail.com>
2024-06-22 14:39:13 -0500

    fix: disable esbuild if not configured to enable correct sourcemaps (#1177)

--- packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
@@ -161,6 +161,7 @@ export function angular(options?: PluginOptions): Plugin[] {
           );
 
         return {
+          esbuild: config.esbuild ?? false,
           optimizeDeps: {
             include: ['rxjs/operators', 'rxjs'],
             exclude: ['@angular/platform-server'],
@@ -363,10 +364,8 @@ export function angular(options?: PluginOptions): Plugin[] {
 
           if (jit) {
             return {
-              code: data.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, ''),
-              map: {
-                mappings: '',
-              },
+              code: data,
+              map: null,
             };
           }
 
@@ -391,10 +390,8 @@ export function angular(options?: PluginOptions): Plugin[] {
 
           if (!forceAsyncTransformation && !isProd) {
             return {
-              code: data.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, ''),
-              map: {
-                mappings: '',
-              },
+              code: data,
+              map: null,
             };
           }
 
