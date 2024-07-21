d9c7f80554633ce58c0656540581e8bb654dfbac
Brandon Roberts <robertsbt@gmail.com>
2024-07-21 17:59:32 -0500

    feat(create-analog): add request context setup to templates

--- packages/create-analog/template-latest/src/main.server.ts
+++ packages/create-analog/template-latest/src/main.server.ts
@@ -1,9 +1,10 @@
 import 'zone.js/node';
 import '@angular/platform-server/init';
-
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
+import { providerServerContext } from '@analogjs/router/server';
+import { ServerContext } from '@analogjs/router/tokens';
 
 import { config } from './app/app.config.server';
 import { AppComponent } from './app/app.component';
@@ -16,10 +17,15 @@ export function bootstrap() {
   return bootstrapApplication(AppComponent, config);
 }
 
-export default async function render(url: string, document: string) {
+export default async function render(
+  url: string,
+  document: string,
+  serverContext: ServerContext
+) {
   const html = await renderApplication(bootstrap, {
     document,
     url,
+    platformProviders: [providerServerContext(serverContext)],
   });
 
   return html;
