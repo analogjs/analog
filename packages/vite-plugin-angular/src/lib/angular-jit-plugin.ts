f46c582b53789ee13372c8dccc2d0157797ca88d
Brandon Roberts <robertsbt@gmail.com>
2024-08-01 19:28:53 -0500

    fix(vite-plugin-angular): use Vite preprocessCSS function for CSS transform

--- packages/vite-plugin-angular/src/lib/angular-jit-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-jit-plugin.ts
@@ -1,31 +1,16 @@
-import { Plugin, PluginContainer, ViteDevServer } from 'vite';
+import { Plugin, preprocessCSS } from 'vite';
 
 export function jitPlugin({
   inlineStylesExtension,
 }: {
   inlineStylesExtension: string;
 }): Plugin {
-  let styleTransform: PluginContainer['transform'] | undefined;
-  let watchMode = false;
-  let viteServer: ViteDevServer | undefined;
-  let cssPlugin: Plugin | undefined;
+  let config: any;
 
   return {
     name: '@analogjs/vite-plugin-angular-jit',
-    config(_config, { command }) {
-      watchMode = command === 'serve';
-    },
-    buildStart({ plugins }) {
-      if (Array.isArray(plugins)) {
-        cssPlugin = plugins.find((plugin) => plugin.name === 'vite:css');
-      }
-
-      styleTransform = watchMode
-        ? viteServer!.pluginContainer.transform
-        : (cssPlugin!.transform as PluginContainer['transform']);
-    },
-    configureServer(server) {
-      viteServer = server;
+    config(_config) {
+      config = _config;
     },
     resolveId(id: string) {
       if (id.startsWith('virtual:angular')) {
@@ -46,9 +31,10 @@ export function jitPlugin({
         let styles: string | undefined = '';
 
         try {
-          const compiled = await styleTransform!(
+          const compiled = await preprocessCSS(
             decodedStyles,
-            `${styleId}.${inlineStylesExtension}?direct`
+            `${styleId}.${inlineStylesExtension}?direct`,
+            config
           );
           styles = compiled?.code;
         } catch (e) {
