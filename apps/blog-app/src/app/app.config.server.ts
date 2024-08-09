d540987dbe4b0f737c0811843e608c281ed7a320
Brandon Roberts <robertsbt@gmail.com>
2024-08-09 08:16:37 -0500

    chore: add example ts/analog page routes from shared libraries to blog-app (#1266)

--- apps/blog-app/src/app/app.config.server.ts
+++ apps/blog-app/src/app/app.config.server.ts
@@ -1,16 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import {
-  provideServerRendering,
-  ɵSERVER_CONTEXT as SERVER_CONTEXT,
-} from '@angular/platform-server';
+import { provideServerRendering } from '@angular/platform-server';
 
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    { provide: SERVER_CONTEXT, useValue: 'ssr-analog' },
-  ],
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
