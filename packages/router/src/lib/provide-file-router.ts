838646c6d21cf31bb7b6d47b2c0b8bee8f4f3859
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2024-08-13 21:06:53 -0500

    fix(router): handle cookie header internally for server data loading (#1276)

--- packages/router/src/lib/provide-file-router.ts
+++ packages/router/src/lib/provide-file-router.ts
@@ -4,9 +4,11 @@ import {
   makeEnvironmentProviders,
 } from '@angular/core';
 import { provideRouter, RouterFeatures } from '@angular/router';
+import { ɵHTTP_ROOT_INTERCEPTOR_FNS as HTTP_ROOT_INTERCEPTOR_FNS } from '@angular/common/http';
 
 import { routes } from './routes';
 import { updateMetaTagsOnRouteChange } from './meta-tags';
+import { cookieInterceptor } from './cookie-interceptor';
 
 /**
  * Sets up providers for the Angular router, and registers
@@ -25,6 +27,11 @@ export function provideFileRouter(
       provide: ENVIRONMENT_INITIALIZER,
       multi: true,
       useValue: () => updateMetaTagsOnRouteChange(),
+    },
+    {
+      provide: HTTP_ROOT_INTERCEPTOR_FNS,
+      multi: true,
+      useValue: cookieInterceptor,
     },
   ]);
 }
