46da68a2b431dc63501d216eaedffd44e822e203
Brandon Roberts <robertsbt@gmail.com>
2024-08-28 10:20:37 -0500

    fix(vite-plugin-angular): use JavaScript transformer from Angular Devkit for build optimizations (#1302)

--- apps/ng-app/vite.config.ts
+++ apps/ng-app/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     analog({
       ssr: false,
+      static: true,
       vite: {
         experimental: {
           supportAnalogFormat: true,
