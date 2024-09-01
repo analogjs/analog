738092c1161039e7ecd994e75e8500ec9d3c2a95
Brandon Roberts <robertsbt@gmail.com>
2024-09-01 12:47:54 -0500

    chore: add fallback base URL for og image

--- apps/blog-app/src/server/routes/v1/[...slug].ts
+++ apps/blog-app/src/server/routes/v1/[...slug].ts
@@ -1,4 +1,4 @@
-import { defineEventHandler, getQuery } from 'h3';
+import { defineEventHandler, getQuery, getRequestURL } from 'h3';
 
 import { ImageResponse } from '@analogjs/content/og';
 
@@ -8,18 +8,15 @@ export default defineEventHandler(async (event) => {
   );
   const fontData: ArrayBuffer = await fontFile.arrayBuffer();
   const query = getQuery(event); // query params
+  const base = getRequestURL(event).origin;
 
   const template = `
     <div tw="bg-gray-50 flex w-full h-full items-center justify-center">
         <div tw="flex flex-col items-center w-full py-12 px-4 p-8">
           <div tw="flex">
-            <img src="${
-              process.env['VITE_ANALOG_BASE_URL']
-            }/analog.svg" width="600" height="450"/>
+            <img src="${base}/analog.svg" width="600" height="450"/>
 
-            <img src="${
-              process.env['VITE_ANALOG_BASE_URL']
-            }/angular-gradient.png" width="400" height="400"/>
+            <img src="${base}/angular-gradient.png" width="400" height="400"/>
           </div>
           <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
             <span>${query['title'] ? `${query['title']}` : 'Hello World'}</span>
