34afb26ae32c154353791eafeaaf18419604af2f
Brandon Roberts <robertsbt@gmail.com>
2024-09-06 15:49:32 -0500

    Revert "fix(vite-plugin-angular): correctly capture diagnostics from errors and warnings" (#1329)

--- packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
@@ -613,11 +613,11 @@ export function createFileEmitter(
       : [];
 
     const errors = diagnostics
-      .filter((d) => d.category === 1)
+      .filter((d) => d.category === ts.DiagnosticCategory?.Error)
       .map((d) => d.messageText);
 
     const warnings = diagnostics
-      .filter((d) => d.category === 0)
+      .filter((d) => d.category === ts.DiagnosticCategory?.Warning)
       .map((d) => d.messageText);
 
     let content: string | undefined;
