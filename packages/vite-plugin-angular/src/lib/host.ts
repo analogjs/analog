2f5400d2b18efb424034ed7afca010a95bb377d6
Joshua Morony <joshua.morony@gmail.com>
2024-09-10 11:40:04 +0930

    fix(vite-plugin-angular): add check for ag files in fileExists function

--- packages/vite-plugin-angular/src/lib/host.ts
+++ packages/vite-plugin-angular/src/lib/host.ts
@@ -112,7 +112,8 @@ export function augmentHostWithResources(
       if (
         fileName.includes('virtual-analog:') &&
         !fileName.endsWith('analog.d') &&
-        !fileName.endsWith('agx.d')
+        !fileName.endsWith('agx.d') &&
+        !fileName.endsWith('ag.d')
       ) {
         return true;
       }
