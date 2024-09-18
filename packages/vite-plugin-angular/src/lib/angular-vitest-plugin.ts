157d9e272ddce9673bbd3dafb26519c50a7d13f4
Brandon Roberts <robertsbt@gmail.com>
2024-09-18 08:18:49 -0500

    fix(vite-plugin-angular): add better support for angular/cdk in vitest browser testing (#1353)

--- packages/vite-plugin-angular/src/lib/angular-vitest-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-vitest-plugin.ts
@@ -1,4 +1,4 @@
-import { Plugin, UserConfig, transformWithEsbuild } from 'vite';
+import { Plugin, transformWithEsbuild } from 'vite';
 
 export function angularVitestPlugin(): Plugin {
   return {
@@ -14,14 +14,20 @@ export function angularVitestPlugin(): Plugin {
           pool: userConfig.test?.pool ?? 'vmThreads',
           server: {
             deps: {
-              inline: ['@analogjs/router'],
+              inline: [
+                '@analogjs/router',
+                '@analogjs/vitest-angular/setup-zone',
+              ],
             },
           },
         },
       };
     },
     async transform(_code, id) {
-      if (/fesm2022/.test(id) && _code.includes('async (')) {
+      if (
+        (/fesm2022/.test(id) && _code.includes('async (')) ||
+        _code.includes('@angular/cdk')
+      ) {
         const { code, map } = await transformWithEsbuild(_code, id, {
           loader: 'js',
           format: 'esm',
