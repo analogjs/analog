aae93922fecc35695403393a29687b780dae453d
Joshua Morony <joshua.morony@gmail.com>
2024-09-19 15:39:24 +0930

    fix(vite-plugin-angular): use defaultMarkdownTemplateTransform if none supplied

--- apps/ng-app/vite.config.ts
+++ apps/ng-app/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => ({
       vite: {
         experimental: {
           supportAnalogFormat: true,
+          markdownTemplateTransforms: [],
         },
       },
     }),
--- packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-vite-plugin.ts
@@ -114,9 +114,10 @@ export function angular(options?: PluginOptions): Plugin[] {
     supportedBrowsers: options?.supportedBrowsers ?? ['safari 15'],
     jit: options?.experimental?.supportAnalogFormat ? false : options?.jit,
     supportAnalogFormat: options?.experimental?.supportAnalogFormat ?? false,
-    markdownTemplateTransforms:
-      options?.experimental?.markdownTemplateTransforms ??
-      defaultMarkdownTemplateTransforms,
+    markdownTemplateTransforms: options?.experimental
+      ?.markdownTemplateTransforms?.length
+      ? options.experimental.markdownTemplateTransforms
+      : defaultMarkdownTemplateTransforms,
     include: options?.include ?? [],
     additionalContentDirs: options?.additionalContentDirs ?? [],
   };
