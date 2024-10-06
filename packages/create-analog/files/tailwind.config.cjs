c051f056974b2336e209ea6941c42822c808ab81
Brandon Roberts <robertsbt@gmail.com>
2024-10-05 21:48:10 -0500

    fix(create-analog): add Analog SFCs to Tailwind config (#1388)

--- packages/create-analog/files/tailwind.config.cjs
+++ packages/create-analog/files/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,ts,md}'],
+  content: ['./index.html', './src/**/*.{html,ts,md,analog,ag}'],
   theme: {
     extend: {},
   },
