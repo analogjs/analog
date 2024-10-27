f8559e7f83c81e968104cd15699b7a56aa770375
Brandon Roberts <robertsbt@gmail.com>
2024-10-27 14:24:05 -0500

    fix(vite-plugin-angular): infer production build from config mode instead of NODE_ENV

--- packages/vite-plugin-angular/src/lib/angular-build-optimizer-plugin.ts
+++ packages/vite-plugin-angular/src/lib/angular-build-optimizer-plugin.ts
@@ -3,10 +3,8 @@ import { Plugin } from 'vite';
 import { JavaScriptTransformer } from './utils/devkit.js';
 
 export function buildOptimizerPlugin({
-  isProd,
   jit,
 }: {
-  isProd: boolean;
   supportedBrowsers: string[];
   jit: boolean;
 }): Plugin {
@@ -19,11 +17,14 @@ export function buildOptimizerPlugin({
     },
     1
   );
+  let isProd = false;
 
   return {
     name: '@analogjs/vite-plugin-angular-optimizer',
     apply: 'build',
-    config() {
+    config(userConfig) {
+      isProd = userConfig.mode === 'production';
+
       return {
         define: isProd
           ? {
