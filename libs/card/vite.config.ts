db57805311e368eb65076ef5c2a86a3484e0fcd9
Brandon Roberts <robertsbt@gmail.com>
2024-11-15 11:14:05 -0600

    chore: revert builder for card library

---
+++ libs/card/vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    plugins: [angular()],
+    test: {
+      reporters: ['default'],
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cacheDir: '../../node_modules/.vitest',
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
