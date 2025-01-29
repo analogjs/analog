5825760dadf52695faec56e5762ea00e3cb2d46f
Joshua Morony <joshua.morony@gmail.com>
2025-01-29 16:00:31 +1030

    fix(vite-plugin-angular): cache MarkedSetupService creation

--- packages/vite-plugin-angular/src/lib/authoring/markdown-transform.ts
+++ packages/vite-plugin-angular/src/lib/authoring/markdown-transform.ts
@@ -1,17 +1,26 @@
 import { FRONTMATTER_REGEX } from './constants.js';
 import { type VFile } from 'vfile';
+import { type MarkedSetupService } from './marked-setup.service.js';
 
 export type MarkdownTemplateTransform = (
   content: string,
   fileName: string
 ) => string | Promise<string> | Promise<VFile>;
 
+let markedSetupServicePromise: undefined | Promise<MarkedSetupService>;
+
 export const defaultMarkdownTemplateTransform: MarkdownTemplateTransform =
   async (content: string) => {
-    const { MarkedSetupService } = await import('./marked-setup.service.js');
-
+    if (!markedSetupServicePromise) {
+      // set immediately to prevent other calls from seeing markedSetupServicePromise as
+      // undefined - can't use await here
+      markedSetupServicePromise = import('./marked-setup.service.js').then(
+        ({ MarkedSetupService }) => new MarkedSetupService()
+      );
+    }
     // read template sections, parse markdown
-    const markedSetupService = new MarkedSetupService();
+    const markedSetupService = await markedSetupServicePromise;
+
     const mdContent = markedSetupService
       .getMarkedInstance()
       .parse(
