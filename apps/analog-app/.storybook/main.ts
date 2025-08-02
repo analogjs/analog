09b8e88c917f7c6fea6ebda9271d90c973e69303
Brandon Roberts <robertsbt@gmail.com>
2025-08-01 23:48:28 -0500

    build: add Storybook configuration to analog-app

---
+++ apps/analog-app/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@analogjs/storybook-angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+  framework: {
+    name: '@analogjs/storybook-angular',
+    options: {},
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ apps/analog-app/src/stories/page.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@analogjs/storybook-angular';
+import { within, userEvent, expect } from 'storybook/test';
+
+import { PageComponent } from './page.component';
+
+const meta: Meta<PageComponent> = {
+  title: 'Example/Page',
+  component: PageComponent,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<PageComponent>;
+
+export const LoggedOut: Story = {};
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const LoggedIn: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const loginButton = canvas.getByRole('button', { name: /Log in/i });
+    await expect(loginButton).toBeInTheDocument();
+    await userEvent.click(loginButton);
+    await expect(loginButton).not.toBeInTheDocument();
+
+    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
+  },
+};
