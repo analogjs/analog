5ab0277abbccb841168e805312296c82af2e4d17
Ben Snyder <ben@snyder.tech>
2025-08-16 11:58:32 -0400

    fix: remove 3 instances of @angular/compiler imports

--- apps/ng-app/src/main.ts
+++ apps/ng-app/src/main.ts
@@ -1,5 +1,4 @@
 import 'zone.js';
-import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import AppComponent from './app/app.component.ag';
