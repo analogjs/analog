d63da782cf9369ad6ed9dad99fb6407d094a7016
Brandon Roberts <robertsbt@gmail.com>
2025-08-20 21:49:44 -0500

    feat(vite-plugin-angular): add Angular builders for Vite (#1846)

---
+++ packages/vite-plugin-angular-tools/src/builders/vite/schema.d.ts
@@ -0,0 +1,6 @@
+export interface ViteBuildSchema {
+  configFile?: string;
+  outputPath: string;
+  sourcemap?: boolean;
+  mode?: string;
+}
---
+++ packages/vite-plugin-angular-tools/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    test: {
+      reporters: ['default'],
+      globals: true,
+      include: ['**/*.spec.ts'],
+      exclude: ['**/files/**/*.spec.ts'],
+      cacheDir: '../../node_modules/.vitest',
+      testTimeout: 10000,
+      passWithNoTests: true,
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
