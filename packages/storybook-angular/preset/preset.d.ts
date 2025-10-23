ac591bef9756cfcfcec6f35bd4bc6d04f4e1658f
Brandon Roberts <robertsbt@gmail.com>
2025-10-23 08:36:48 -0500

    fix(storybook-angular): use preset.mjs for Windows support

---
+++ packages/storybook-angular/preset/preset.d.ts
@@ -0,0 +1,10 @@
+export declare const previewAnnotations: (
+  entries: any[],
+  options: any,
+) => Promise<any[]>;
+export declare const core: (config: any, options: any) => Promise<any>;
+export declare const viteFinal: (
+  config: any,
+  options: any,
+) => Promise<Record<string, any>>;
+export { addons } from '@storybook/angular/preset';
