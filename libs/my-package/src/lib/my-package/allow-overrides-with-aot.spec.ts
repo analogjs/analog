ef8133810b2dc6356d540acd4f9c493767c6860c
Younes Jaaidi <younes@marmicode.io>
2025-10-27 21:28:07 +0100

    fix(vite-plugin-angular): allow TestBed.overrideXXX() APIs in AOT mode

--- libs/my-package/src/lib/my-package/allow-overrides-with-aot.spec.ts
+++ libs/my-package/src/lib/my-package/allow-overrides-with-aot.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { MyPackage } from './my-package';
 import { test } from 'vitest';
 
-test.fails('overrides should work even when AOT is enabled', () => {
+test('overrides should work even when AOT is enabled', () => {
   /* AOT is enabled through `angular({jit: false})` in `../../../vite.config.ts`. */
 
   TestBed.overrideComponent(MyPackage, {
