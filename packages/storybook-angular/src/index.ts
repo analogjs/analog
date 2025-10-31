f35c8f97b51187453f19102c85e6733fbfb85990
Brandon Roberts <robertsbt@gmail.com>
2025-10-31 14:04:18 -0500

    fix(storybook-angular): update type exports

--- packages/storybook-angular/src/index.ts
+++ packages/storybook-angular/src/index.ts
@@ -1,2 +1,19 @@
-export * from './types';
-export * from '@storybook/angular/client';
+export {
+  type AngularOptions,
+  type Parameters as AngularParameters,
+  type AngularRenderer,
+  type Decorator,
+  type IStory,
+  type Loader,
+  type Meta,
+  type Preview,
+  type StoryContext,
+  type StoryFn,
+  type StoryObj,
+  applicationConfig,
+  argsToTemplate,
+  componentWrapperDecorator,
+  moduleMetadata,
+  setProjectAnnotations,
+} from '@storybook/angular';
+export { type FrameworkOptions, type StorybookConfig } from './types';
