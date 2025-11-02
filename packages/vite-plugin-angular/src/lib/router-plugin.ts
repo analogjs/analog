43395f06256c069c57b448e310030a43132e9879
Brandon Roberts <robertsbt@gmail.com>
2025-11-02 16:36:54 -0600

    fix(vite-plugin-angular): expand optimization for Angular packages

--- packages/vite-plugin-angular/src/lib/router-plugin.ts
+++ packages/vite-plugin-angular/src/lib/router-plugin.ts
@@ -3,11 +3,14 @@ import { JavaScriptTransformer } from './utils/devkit.js';
 export function routerPlugin() {
   const javascriptTransformer = new JavaScriptTransformer({ jit: true }, 1);
 
+  /**
+   * Transforms Angular packages the didn't get picked up by Vite's pre-optimization.
+   */
   return {
     name: 'analogjs-router-optimization',
     enforce: 'pre',
     async transform(_code: string, id: string) {
-      if (id.includes('analogjs-') && id.includes('.mjs')) {
+      if (id.includes('fesm') && id.includes('.mjs')) {
         const path = id.split('?')[0];
         const contents = await javascriptTransformer.transformFile(path);
 
