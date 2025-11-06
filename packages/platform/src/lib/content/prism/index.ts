9c637265eb1e4403b825656beb03c83139ab9d4a
Brandon Roberts <robertsbt@gmail.com>
2025-11-05 22:01:30 -0600

    perf(platform): use singleton instances for marked and highlighters for content (#1935)

--- packages/platform/src/lib/content/prism/index.ts
+++ packages/platform/src/lib/content/prism/index.ts
@@ -2,6 +2,9 @@ import { PrismHighlighter } from './prism-highlighter.js';
 
 export { PrismHighlighter };
 
+let highlighterInstance: PrismHighlighter;
+
 export function getPrismHighlighter() {
-  return new PrismHighlighter();
+  highlighterInstance ??= new PrismHighlighter();
+  return highlighterInstance;
 }
