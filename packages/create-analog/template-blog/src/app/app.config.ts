90ac7e246f93294fc585bbefc28b93e8b81746ea
Brandon Roberts <robertsbt@gmail.com>
2025-11-25 07:13:16 -0600

    fix(create-analog): update blog template to zoneless

--- packages/create-analog/template-blog/src/app/app.config.ts
+++ packages/create-analog/template-blog/src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
 } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
@@ -16,7 +15,6 @@ import { __HIGHLIGHTER__ } from '@analogjs/content/__HIGHLIGHTER_ENTRY_POINT__';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(),
     provideHttpClient(
       withFetch(),
