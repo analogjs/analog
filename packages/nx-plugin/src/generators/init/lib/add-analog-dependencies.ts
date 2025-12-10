42a963025bd31cc6279e9d135d7cd7cce894e00c
Brandon Roberts <robertsbt@gmail.com>
2025-12-09 21:49:54 -0600

    fix(nx-plugin): pass Nx version for package version detection

--- packages/nx-plugin/src/generators/init/lib/add-analog-dependencies.ts
+++ packages/nx-plugin/src/generators/init/lib/add-analog-dependencies.ts
@@ -3,8 +3,12 @@ import { addDependenciesToPackageJson, Tree } from '@nx/devkit';
 import { getAnalogDevDependencies } from '../../../utils/versions/dev-dependencies';
 import { getAnalogDependencies } from '../../../utils/versions/dependencies';
 
-export function addAnalogDependencies(tree: Tree, angularVersion: string) {
-  const devDependencies = getAnalogDevDependencies(angularVersion);
+export function addAnalogDependencies(
+  tree: Tree,
+  angularVersion: string,
+  nxVersion?: string,
+) {
+  const devDependencies = getAnalogDevDependencies(angularVersion, nxVersion);
   const dependencies = getAnalogDependencies(angularVersion);
 
   addDependenciesToPackageJson(
