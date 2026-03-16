0e52db533eeb0c7a05758507f5896d56f2f38c00
Ben Snyder <ben@snyder.tech>
2026-03-16 10:32:19 -0400

    chore: update project.json to add build dependency

--- packages/router/vite.config.ts
+++ packages/router/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       viteTsConfigPaths({
         root: '../../',
-        projects: [],
+        projects: ['tsconfig.base.json'],
       }),
     ],
     test: {
