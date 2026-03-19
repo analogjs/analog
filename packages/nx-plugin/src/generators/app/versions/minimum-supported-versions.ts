8f8883d21dda7865001e0135a7c37e44dee997fe
Ben Snyder <ben@snyder.tech>
2026-03-19 16:16:11 -0400

    fix(nx-plugin): make generator and build configs work on Windows CI

--- packages/nx-plugin/src/generators/app/versions/minimum-supported-versions.ts
+++ packages/nx-plugin/src/generators/app/versions/minimum-supported-versions.ts
@@ -1,5 +1,4 @@
 import { lt, coerce } from 'semver';
-export const MINIMUM_SUPPORTED_NX_TRPC_VERSION = '16.1.0';
 export const MINIMUM_SUPPORTED_NX_VERSION = '15.2.0';
 export const MINIMUM_SUPPORTED_ANGULAR_VERSION = '15.0.0';
 export const belowMinimumSupportedNxVersion = (nxVersion: string): boolean =>
