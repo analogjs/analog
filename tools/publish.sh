5bc59353e0a2b4b08fac23ae7f62dcedf3aae770
Ben Snyder <ben@snyder.tech>
2026-03-26 19:43:50 -0400

    chore: add release artifact management and smoke testing scripts

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 echo "Publishing to $RELEASE_TAG"
-npm publish packages/astro-angular/dist --tag $RELEASE_TAG
-npm publish packages/content/dist --tag $RELEASE_TAG
-npm publish packages/platform/dist --tag $RELEASE_TAG
-npm publish packages/router/dist --tag $RELEASE_TAG
-npm publish packages/storybook-angular/dist --tag $RELEASE_TAG
-npm publish packages/vite-plugin-angular/dist --tag $RELEASE_TAG
-npm publish packages/vite-plugin-nitro/dist --tag $RELEASE_TAG
-npm publish packages/vitest-angular/dist --tag $RELEASE_TAG
-npm publish dist/packages/create-analog --tag $RELEASE_TAG
+node tools/scripts/release-artifacts.mts publish --tag "$RELEASE_TAG"
