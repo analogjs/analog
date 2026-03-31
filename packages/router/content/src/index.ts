1d068860a2dba9401b2be490adfede0f83c657c4
Ben Snyder <ben@snyder.tech>
2026-03-31 16:59:05 -0400

    feat(router): add injectDebugContentRoutes for enhanced debugging capabilities

--- packages/router/content/src/index.ts
+++ packages/router/content/src/index.ts
@@ -1,3 +1,4 @@
 export { withContentRoutes } from './lib/with-content-routes';
 export { ANALOG_CONTENT_ROUTE_FILES, createContentRoutes } from './lib/routes';
 export type { Files } from './lib/routes';
+export { injectDebugContentRoutes } from './lib/debug/routes';
