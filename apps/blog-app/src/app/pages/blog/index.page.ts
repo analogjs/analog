bb8502bd6dd05792b032398dd30f480123aab59b
Ben Snyder <ben@snyder.tech>
2026-04-01 11:08:43 -0400

    fix: update blog post route to use direct path instead of route tree

--- apps/blog-app/src/app/pages/blog/index.page.ts
+++ apps/blog-app/src/app/pages/blog/index.page.ts
@@ -2,12 +2,9 @@ import { isPlatformBrowser } from '@angular/common';
 import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { contentFilesResource } from '@analogjs/content/resources';
-import { analogRouteTree } from '../../../routeTree.gen';
 import { routePath } from '@analogjs/router';
 
 import { PostAttributes } from './models';
-
-const blogPostRoute = analogRouteTree.byId['/blog/[slug]'];
 
 @Component({
   standalone: true,
@@ -19,7 +16,7 @@ const blogPostRoute = analogRouteTree.byId['/blog/[slug]'];
       @for (post of contentFilesResource.value(); track post.slug) {
         <li>
           @let postLink =
-            routePath(blogPostRoute.fullPath, {
+            routePath('/blog/[slug]', {
               params: { slug: post.slug },
             });
           <a [routerLink]="postLink.path"> {{ post.attributes.title }}</a>
@@ -30,7 +27,6 @@ const blogPostRoute = analogRouteTree.byId['/blog/[slug]'];
 })
 export default class BlogComponent implements OnInit {
   readonly routePath = routePath;
-  readonly blogPostRoute = blogPostRoute;
   private readonly platformId = inject(PLATFORM_ID);
   private readonly router = inject(Router);
   readonly contentFilesResource = contentFilesResource<PostAttributes>(
