2069ba28533301a5d553677106b2688b7a6486a1
Ben Snyder <ben@snyder.tech>
2026-04-05 10:01:03 -0400

    fix: re-enable redirect to /blog in index route

--- apps/blog-app/src/app/pages/index.page.ts
+++ apps/blog-app/src/app/pages/index.page.ts
@@ -1,20 +1,6 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouteMeta } from '@analogjs/router';
 
-@Component({
-  standalone: true,
-  template: `<p><a href="/blog">Redirecting to the blog...</a></p>`,
-})
-export default class BlogIndexPageComponent implements OnInit {
-  private readonly platformId = inject(PLATFORM_ID);
-  private readonly router = inject(Router);
-
-  ngOnInit() {
-    if (!isPlatformBrowser(this.platformId)) {
-      return;
-    }
-
-    void this.router.navigateByUrl('/blog', { replaceUrl: true });
-  }
-}
+export const routeMeta: RouteMeta = {
+  redirectTo: '/blog',
+  pathMatch: 'full',
+};
